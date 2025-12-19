fcadafd8178526a2b3f29cd7d3e78b9cce87f374
Jamilyasbarb <mily.yasbarb@gmail.com>
2025-12-19 14:28:51 -0300

    FEAT: added service and repository for the task entity.

---
+++ src/main/java/br/com/alura/AluraFake/repositories/TaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.alura.AluraFake.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.scheduling.config.Task;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}

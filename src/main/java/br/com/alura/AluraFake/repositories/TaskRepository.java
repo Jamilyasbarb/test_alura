15644bb16669ff9067919ce8b2dfb022cd9469b3
Jamilyasbarb <mily.yasbarb@gmail.com>
2025-12-19 17:21:00 -0300

    FEAT: added query to return course questions with the same statement

--- src/main/java/br/com/alura/AluraFake/repositories/TaskRepository.java
+++ src/main/java/br/com/alura/AluraFake/repositories/TaskRepository.java
@@ -1,9 +1,20 @@
 package br.com.alura.AluraFake.repositories;
 
+import br.com.alura.AluraFake.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.scheduling.config.Task;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByCourseId(Long courseId);
+
+    @Query("""
+            SELECT t FROM Task t WHERE t.course.id = :courseId AND t.statement = :statement
+            """)
+    List<Task> findTaskAlreadyUtilizedByCourseId(String statement, Long courseId);
 }

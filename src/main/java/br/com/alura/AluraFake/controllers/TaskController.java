a3b32e9e146438df5d151b6255a710e8785f341b
Jamilyasbarb <mily.yasbarb@gmail.com>
2025-12-19 17:30:03 -0300

    FEAT: added endpoint for tasks with cursive answers.

--- src/main/java/br/com/alura/AluraFake/controllers/TaskController.java
+++ src/main/java/br/com/alura/AluraFake/controllers/TaskController.java
@@ -1,23 +1,32 @@
 package br.com.alura.AluraFake.controllers;
 
+import br.com.alura.AluraFake.domain.Task;
+import br.com.alura.AluraFake.dto.task.CreateTaskDTO;
+import br.com.alura.AluraFake.services.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/task")
 public class TaskController {
 
-    @PostMapping("/task/new/opentext")
-    public ResponseEntity newOpenTextExercise() {
+    @Autowired
+    private TaskService taskService;
 
-        return ResponseEntity.ok().build();
+    @PostMapping("/new/opentext")
+    public ResponseEntity newOpenTextExercise(@Valid @RequestBody CreateTaskDTO createTaskDTO) {
+        Task task = taskService.createTask(createTaskDTO);
+        return ResponseEntity.ok().body(task);
     }
 
-    @PostMapping("/task/new/singlechoice")
+    @PostMapping("/new/singlechoice")
     public ResponseEntity newSingleChoice() {
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/task/new/multiplechoice")
+    @PostMapping("/new/multiplechoice")
     public ResponseEntity newMultipleChoice() {
         return ResponseEntity.ok().build();
     }

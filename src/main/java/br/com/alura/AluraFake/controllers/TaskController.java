90e660f3acc9add5a23bb24172ae4fe4aeb7b384
Jamilyasbarb <mily.yasbarb@gmail.com>
2025-12-20 13:47:46 -0300

    FIX: fixed HTTP status returned when creating tasks

--- src/main/java/br/com/alura/AluraFake/controllers/TaskController.java
+++ src/main/java/br/com/alura/AluraFake/controllers/TaskController.java
@@ -5,6 +5,7 @@ import br.com.alura.AluraFake.dto.task.CreateTaskDTO;
 import br.com.alura.AluraFake.services.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,19 +19,19 @@ public class TaskController {
     @PostMapping("/new/opentext")
     public ResponseEntity newOpenTextExercise(@Valid @RequestBody CreateTaskDTO createTaskDTO) {
         Task task = taskService.createTask(createTaskDTO);
-        return ResponseEntity.ok().body(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
 
     @PostMapping("/new/singlechoice")
     public ResponseEntity newSingleChoice(@Valid @RequestBody CreateTaskDTO createTaskDTO) {
         Task task = taskService.createTaskOneChoice(createTaskDTO, false);
-        return ResponseEntity.ok().body(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
 
     @PostMapping("/new/multiplechoice")
     public ResponseEntity newMultipleChoice(@Valid @RequestBody CreateTaskDTO createTaskDTO) {
         Task task = taskService.createTaskOneChoice(createTaskDTO, true);
-        return ResponseEntity.ok().body(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
 
 }

ff087e77f721989551ffb39c48ed00c98bd6e530
Jamilyasbarb <mily.yasbarb@gmail.com>
2025-12-20 11:54:20 -0300

    FEAT: added query for updating task order, validating existence, and returning last task ID

--- src/main/java/br/com/alura/AluraFake/repositories/TaskRepository.java
+++ src/main/java/br/com/alura/AluraFake/repositories/TaskRepository.java
@@ -3,10 +3,13 @@ package br.com.alura.AluraFake.repositories;
 import br.com.alura.AluraFake.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
@@ -17,4 +20,27 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
             SELECT t FROM Task t WHERE t.course.id = :courseId AND t.statement = :statement
             """)
     List<Task> findTaskAlreadyUtilizedByCourseId(String statement, Long courseId);
+
+    Optional<Task> findByOrder(Integer order);
+
+    @Transactional
+    @Modifying
+    @Query(value = """
+            UPDATE Task SET order_number = order_number + 1 WHERE order_number >= :orderNumber
+            """, nativeQuery = true)
+    void updateOrder(Integer orderNumber);
+
+    @Query(
+            value = "SELECT EXISTS (SELECT 1 FROM Task)",
+            nativeQuery = true
+    )
+    int existsAnyTask();
+
+    @Query(
+            value = "SELECT MAX(id) FROM Task",
+            nativeQuery = true
+    )
+    Long findLastTaskId();
+
+
 }
